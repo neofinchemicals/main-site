835f14627a932bc7e9005b8270ae56918860b315
jignesh <info@neofinchemicals.com>
2025-03-15 13:32:17 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "../components/common/Navbar";
 import Hero from "../components/home/Hero";
 import CompanyProfile from "../components/home/CompanyProfile";
 import MissionVision from "../components/home/MissionVision";
 import ProductCategories from "../components/home/ProductCategories";
-import Footer from "../components/common/Footer";
 import WhatsAppButton from "../components/common/WhatsAppButton";
 
 const Home = () => {
